5bc583fc9688700e1c5d992a4359c2483ae59969
Nived Krishna <100478612+NivedKris@users.noreply.github.com>
2024-01-13 14:11:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def classify():
 def restart_system():
     # Add any cleanup or reset code here
     return redirect(url_for('home'))
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=False)  # Set debug mode to False for production
